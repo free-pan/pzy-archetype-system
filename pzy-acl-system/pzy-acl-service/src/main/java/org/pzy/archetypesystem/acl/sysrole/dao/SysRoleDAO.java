fada28f5a7513a93c9f842d3c6629b1cd8dc114c
free pan <sd4015700@126.com>
2020-04-03 16:06:10 +0800

    refactor: 代码优化

---
+++ pzy-acl-system/pzy-acl-service/src/main/java/org/pzy/archetypesystem/acl/sysrole/dao/SysRoleDAO.java
@@ -0,0 +1,16 @@
+package org.pzy.archetypesystem.acl.sysrole.mapper;
+
+import org.pzy.archetypesystem.acl.sysrole.entity.SysRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * sys_role 表DAO接口
+ *
+ * @author pan
+ * @since 2020-04-03
+ */
+@Repository
+public interface SysRoleDAO extends BaseMapper<SysRole> {
+
+}
