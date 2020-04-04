01893925a3a0d5d75967e7d47b2ad42f9aefaa1b
free pan <sd4015700@126.com>
2020-04-04 20:59:15 +0800

    refactor: 更新pzy-swagger-ui的ui主题

--- pzy-acl-system/pzy-acl-service/src/main/java/org/pzy/archetypesystem/acl/sysrole/service/impl/SysRoleServiceImpl.java
+++ pzy-acl-system/pzy-acl-service/src/main/java/org/pzy/archetypesystem/acl/sysrole/service/impl/SysRoleServiceImpl.java
@@ -1,10 +1,12 @@
 package org.pzy.archetypesystem.acl.sysrole.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.pzy.archetypesystem.acl.sysrole.dao.SysRoleDAO;
 import org.pzy.archetypesystem.acl.sysrole.entity.SysRole;
 import org.pzy.archetypesystem.acl.sysrole.service.SysRoleService;
 import org.pzy.opensource.mybatisplus.service.ServiceTemplateImpl;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
  * @author pan
  * @since 2020-04-03
  */
+@Slf4j
 @Service
 @Validated
 @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
@@ -23,4 +26,12 @@ import org.springframework.validation.annotation.Validated;
 public class SysRoleServiceImpl extends ServiceTemplateImpl<SysRoleDAO, SysRole> implements SysRoleService {
 
     public static final String CACHE_NAME = "ACL";
+
+    @CacheEvict(allEntries = true, beforeInvocation = true)
+    @Override
+    public void clearCache() {
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("清除[%s]服务类缓存!", this.getClass().getName()));
+        }
+    }
 }
