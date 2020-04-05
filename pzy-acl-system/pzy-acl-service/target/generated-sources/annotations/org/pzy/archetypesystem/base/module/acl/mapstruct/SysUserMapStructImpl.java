222a2af2211d4d2d9be3456760880574a7e83a96
free pan <sd4015700@126.com>
2020-04-05 15:14:47 +0800

    refactor: 更新pzy-swagger-ui的ui主题

--- pzy-acl-system/pzy-acl-service/target/generated-sources/annotations/org/pzy/archetypesystem/base/module/acl/mapstruct/SysUserMapStructImpl.java
+++ pzy-acl-system/pzy-acl-service/target/generated-sources/annotations/org/pzy/archetypesystem/base/module/acl/mapstruct/SysUserMapStructImpl.java
@@ -1,5 +1,9 @@
 package org.pzy.archetypesystem.base.module.acl.mapstruct;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.Generated;
 import org.pzy.archetypesystem.base.module.acl.dto.SysUserAddDTO;
 import org.pzy.archetypesystem.base.module.acl.dto.SysUserEditDTO;
 import org.pzy.archetypesystem.base.module.acl.dto.SysUserSearchDTO;
@@ -9,16 +13,9 @@ import org.pzy.opensource.comm.mapstruct.StringDataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Generated;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-05T14:32:44+0800",
+    date = "2020-04-05T15:14:29+0800",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_181 (Oracle Corporation)"
 )
 @Component
@@ -104,15 +101,9 @@ public class SysUserMapStructImpl implements SysUserMapStruct {
         if ( source.getDisabled() != null ) {
             sysUserVO.setDisabled( source.getDisabled().intValue() );
         }
-        if ( source.getDisabledTime() != null ) {
-            sysUserVO.setDisabledTime( LocalDateTime.ofInstant( source.getDisabledTime().toInstant(), ZoneId.of( "UTC" ) ) );
-        }
-        if ( source.getCreateTime() != null ) {
-            sysUserVO.setCreateTime( LocalDateTime.ofInstant( source.getCreateTime().toInstant(), ZoneId.of( "UTC" ) ) );
-        }
-        if ( source.getEditTime() != null ) {
-            sysUserVO.setEditTime( LocalDateTime.ofInstant( source.getEditTime().toInstant(), ZoneId.of( "UTC" ) ) );
-        }
+        sysUserVO.setDisabledTime( source.getDisabledTime() );
+        sysUserVO.setCreateTime( source.getCreateTime() );
+        sysUserVO.setEditTime( source.getEditTime() );
         sysUserVO.setCreatorId( source.getCreatorId() );
         sysUserVO.setEditorId( source.getEditorId() );
         sysUserVO.setCreatorName( stringDataMapper.stringMapper( source.getCreatorName() ) );
