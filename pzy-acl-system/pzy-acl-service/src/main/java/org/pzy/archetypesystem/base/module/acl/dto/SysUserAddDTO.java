2272f8888a2556699149e3cacccb1a1a987bc9c2
free pan <sd4015700@126.com>
2020-04-06 08:37:49 +0800

    refactor: 代码优化

--- pzy-acl-system/pzy-acl-service/src/main/java/org/pzy/archetypesystem/base/module/acl/dto/SysUserAddDTO.java
+++ pzy-acl-system/pzy-acl-service/src/main/java/org/pzy/archetypesystem/base/module/acl/dto/SysUserAddDTO.java
@@ -5,7 +5,10 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 /**
@@ -20,12 +23,17 @@ import java.io.Serializable;
 @ApiModel
 public class SysUserAddDTO implements Serializable {
 
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "姓名")
+    @NotBlank(message = "请输入姓名!")
+    @Length(max = 200, message = "请输入{max}字符以内的姓名!")
     private String name;
 
     @ApiModelProperty(value = "邮箱")
+    @NotBlank(message = "请输入邮箱!")
+    @Email(message = "请输入正确的邮箱地址!")
+    @Length(max = 200, message = "请输入{max}字符以内的邮箱!")
     private String email;
 
 }
--- pzy-acl-system/pzy-acl-service/src/test/java/org/pzy/archetypesystem/base/module/acl/service/impl/SysUserServiceImplTest.java
+++ pzy-acl-system/pzy-acl-service/src/test/java/org/pzy/archetypesystem/base/module/acl/service/impl/SysUserServiceImplTest.java
@@ -2,6 +2,7 @@ package org.pzy.archetypesystem.base.module.acl.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.pzy.archetypesystem.base.ApiTestApp;
+import org.pzy.archetypesystem.base.module.acl.dto.SysUserAddDTO;
 import org.pzy.archetypesystem.base.module.acl.service.SysUserService;
 import org.pzy.archetypesystem.base.module.acl.vo.SysUserVO;
 import org.pzy.opensource.domain.PageT;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import javax.validation.ConstraintViolationException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * SysUserServiceImplTest
@@ -36,8 +40,108 @@ public class SysUserServiceImplTest extends AbstractTestNGSpringContextTests {
         assert result.getList().isEmpty();
     }
 
-    @Test
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto: 不能为null")
     public void testSaveAndClearCache() {
+        sysUserService.saveAndClearCache(null);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入邮箱!, saveAndClearCache.dto.name: 请输入姓名!")
+    public void testSaveAndClearCache01() {
+        sysUserService.saveAndClearCache(new SysUserAddDTO());
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入邮箱!")
+    public void testSaveAndClearCache02() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        dto.setName("张三");
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.name: 请输入200字符以内的姓名!, saveAndClearCache.dto.email: 请输入邮箱!")
+    public void testSaveAndClearCache03() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "";
+        for (int i = 0; i < 201; i++) {
+            name += "a";
+        }
+        dto.setName(name);
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入邮箱!, saveAndClearCache.dto.name: 请输入200字符以内的姓名!")
+    public void testSaveAndClearCache04() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = " ";
+        for (int i = 0; i < 200; i++) {
+            name += "a";
+        }
+        dto.setName(name);
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入邮箱!, saveAndClearCache.dto.name: 请输入200字符以内的姓名!")
+    public void testSaveAndClearCache05() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "";
+        for (int i = 0; i < 200; i++) {
+            name += "a";
+        }
+        name += " ";
+        dto.setName(name);
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入邮箱!")
+    public void testSaveAndClearCache06() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "";
+        for (int i = 0; i < 200; i++) {
+            name += "a";
+        }
+        dto.setName(name);
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入正确的邮箱地址!")
+    public void testSaveAndClearCache07() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "zs";
+        dto.setName(name);
+        dto.setEmail("test");
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入正确的邮箱地址!")
+    public void testSaveAndClearCache08() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "zs";
+        dto.setName(name);
+        dto.setEmail("test@");
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class, expectedExceptionsMessageRegExp = "saveAndClearCache.dto.email: 请输入正确的邮箱地址!")
+    public void testSaveAndClearCache09() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "zs";
+        dto.setName(name);
+        dto.setEmail("@test");
+        sysUserService.saveAndClearCache(dto);
+    }
+
+    @Test
+    public void testSaveAndClearCache10() {
+        SysUserAddDTO dto = new SysUserAddDTO();
+        String name = "zs";
+        dto.setName(name);
+        dto.setEmail("sd4015700@126.com");
+        sysUserService.saveAndClearCache(dto);
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("主进程结束!");
     }
 
     @Test
