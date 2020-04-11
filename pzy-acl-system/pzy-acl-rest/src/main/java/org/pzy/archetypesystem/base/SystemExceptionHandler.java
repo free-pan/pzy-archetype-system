120dfc5417c2fc94b446305f4b5b92046700b594
free pan <sd4015700@126.com>
2020-04-11 21:02:06 +0800

    refactor: 优化统一异常处理

--- pzy-acl-system/pzy-acl-rest/src/main/java/org/pzy/archetypesystem/base/SystemExceptionHandler.java
+++ pzy-acl-system/pzy-acl-rest/src/main/java/org/pzy/archetypesystem/base/SystemExceptionHandler.java
@@ -1,6 +1,12 @@
 package org.pzy.archetypesystem.base;
 
-import org.pzy.opensource.springboot.errorhandler.WinterExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.pzy.opensource.springboot.errorhandler.DefaultWinterExceptionHandlerImpl;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * SystemExceptionHandler
@@ -8,5 +14,13 @@ import org.pzy.opensource.springboot.errorhandler.WinterExceptionHandler;
  * @author pan
  * @date 4/11/20
  */
-public class SystemExceptionHandler extends WinterExceptionHandler {
+@Slf4j
+@RestControllerAdvice
+public class SystemExceptionHandler extends DefaultWinterExceptionHandlerImpl {
+    @Override
+    public void customExtendsExceptoinHandler(HttpServletRequest req, HttpServletResponse response, Exception e) {
+        if (e instanceof HttpMessageNotReadableException) {
+            
+        }
+    }
 }
